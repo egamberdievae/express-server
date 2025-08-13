4c22cca69e9656f1cbc093f6d2f657aeadd0e147
Sunniybeliy <sunves080706@gmail.com>
2025-08-13 13:33:36 +0500

    after upgrade

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Super server started");
+const express = require( "express" )
+const app = express()
+
+app.get( "/health", ( req,res ) => res.send( {message: "ok"} ) )
+
+app.listen(3000,() => console.info( "Server running at:3000" ))
